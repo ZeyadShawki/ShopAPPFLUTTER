e70b35d581f0ca6522d885f943be793926833a92
ZeyadShawki <110336659+ZeyadShawki@users.noreply.github.com>
2022-07-31 14:51:41 +0200

    Delete lib/layout/modules directory
    unused modules

--- lib/layout/modules/counter/cubit/states.dart
+++
@@ -1,7 +0,0 @@
-abstract class Counterstate{}
-
-class CounterIntialState extends Counterstate{}
-
-class CounterchangeState extends Counterstate{}
-class CounterplusState extends Counterstate{}
-class CounterminusState extends Counterstate{}
